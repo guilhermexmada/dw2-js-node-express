6a194fa972e4ae7c1ef2e87b63a5f57187b96e58
guilhermexmada <guilhermeshimadapereira@gmail.com>
2025-09-15 18:45:13 -0300

    lista de exercicios 3

---
+++ JS/exercicios/lista-03/ex1/script.js
@@ -0,0 +1,10 @@
+// 03
+function darkMode(){
+    document.querySelector(".container").style.backgroundColor = "#161c22";
+    document.querySelector(".container").style.color = "lightblue";
+}
+
+function lightMode(){
+    document.querySelector(".container").style.backgroundColor = "lightblue";
+    document.querySelector(".container").style.color = "#161c22";
+}
---
+++ JS/exercicios/lista-03/ex2/script.js
@@ -0,0 +1,45 @@
+
+// 01
+const data = new Date();
+document.write(`<p> Exibindo a data atual completa: </p>`);
+document.write(`<p> ${data} </p> <hr>`);
+
+// 02
+let promptValor = prompt(`Digite o valor da sua compra: `);
+let verificarValor = Number(promptValor);
+let compraInternacional;
+
+if(isNaN(verificarValor) == true){
+    document.write(`Valor Inválido!`);
+} else{
+    compraInternacional = verificarValor;
+}
+
+const compraInternacionalDolar = compraInternacional.toLocaleString("en", {
+    style: "currency",
+    currency: "USD"
+});
+document.write(`<p> O valor da sua compra é: ${compraInternacionalDolar}. </p>`);
+
+// 03
+const conversaoReal = compraInternacional * 5.42;
+const compraInternacionalReal = conversaoReal.toLocaleString("pt-br", {
+    style: "currency",
+    currency: "BRL"
+});
+document.write(`<p> 
+    O valor da sua compra convertida para real,
+    na cotação de 2025-09-05 (R$5,42 = $1) é de: ${compraInternacionalReal}. 
+    </p> <hr>`);
+
+// 04
+const formatarEntrega = new Date(data);
+formatarEntrega.setDate(data.getDate() + 12);
+
+const dataEntrega = formatarEntrega.toLocaleDateString("pt-br", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric"
+});
+
+document.write(`<p> A data estimada da sua entrega é: ${dataEntrega} </p>`);
