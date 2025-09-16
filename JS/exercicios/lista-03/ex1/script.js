ae6cfef05f8106ea1143c7e77d1d183bee88a6dd
guilhermexmada <guilhermeshimadapereira@gmail.com>
2025-09-15 22:55:48 -0300

    lista de exercicios 3

--- JS/exercicios/lista-03/ex1/script.js
+++ JS/exercicios/lista-03/ex1/script.js
@@ -1,10 +1,11 @@
-// 03
 function darkMode(){
-    document.querySelector(".container").style.backgroundColor = "#161c22";
-    document.querySelector(".container").style.color = "lightblue";
+    document.querySelector(".container").style.backgroundColor = "#030712";
+    document.querySelector(".container").style.color = "#f8fafc";
 }
 
 function lightMode(){
-    document.querySelector(".container").style.backgroundColor = "lightblue";
+    document.querySelector(".container").style.backgroundColor = "#f8fafc";
     document.querySelector(".container").style.color = "#161c22";
+    document.querySelector(".simbol").style.backgroundColor = "blue";
+    document.querySelector(".simbol").style.animation = "night 2s";
 }
